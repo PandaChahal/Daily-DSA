52e741c078ec83af39e624b46334687c0115d808
Hashandeep-kaur <hkaur4_be22@thapar.edu>
2024-09-06 22:33:17 +0530

    6Sept

---
+++ September6/factorial.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    if (n <= 1) 
+    {
+        return 1; 
+    }  
+    int factorial = n*fact(n-1);
+    return factorial; 
+}
+int main()
+{
+    int n ;
+    cout<<"enter your number : ";
+    cin>>n;
+    //cout<<endl;
+    cout<<"factorial of "<<n<<" is "<<fact(n);
+    return 0;
+}
